2952c653cfe08abebe0206bf3398a6283acc128d
barrera77 <30675869+barrera77@users.noreply.github.com>
2023-08-28 10:47:54 -0600

    Fixed firebase configuration

--- firebase.ts
+++ firebase.ts
@@ -7,14 +7,16 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBmGMqZj-EzMn4iCsEaA0oolf_mo7HCVQ8",
-  authDomain: "nextflix-app-1983b.firebaseapp.com",
-  projectId: "nextflix-app-1983b",
-  storageBucket: "nextflix-app-1983b.appspot.com",
-  messagingSenderId: "774318239700",
-  appId: "1:774318239700:web:8a44042a7dc7456e0b3041",
+  apiKey: "AIzaSyDbaq7U4sF3qPVqK_ItVg-7tyPLTsW-ygc",
+  authDomain: "nextflix-app-5af2d.firebaseapp.com",
+  projectId: "nextflix-app-5af2d",
+  storageBucket: "nextflix-app-5af2d.appspot.com",
+  messagingSenderId: "1043594517725",
+  appId: "1:1043594517725:web:7b32bd517dac2572900f34"
 };
+
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
